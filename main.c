c7a7937fb9291719e2ff207c93aec5a5b4000600
noahweiss890 <nweiss890@gmail.com>
2021-12-13 21:26:37 +0200

    made main

--- main.c
+++ main.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
+#include <string.h>
+
+#define TXT 1024
+#define WORD 30
 
 int main() {
-    printf("Hello, World!\n");
+    char word[WORD + 1];
+    char txt[TXT + 1];
+
+    char c;
+    c = getchar();
+    while(c != ' ' && c != '\n' && c != '\t') {
+        strcat(word, &c);
+        c = getchar();
+    }
+
+    while(c != '~') {
+        strcat(txt, &c);
+        c = getchar();
+    }
+
+    printf("Gematria Sequences: ");
+    func1(word[WORD+1], txt[TXT + 1]);
+
+    printf("Gematria Sequences: ");
+    func2(word[WORD+1], txt[TXT + 1]);
+
+    printf("Gematria Sequences: ");
+    func3(word[WORD+1], txt[TXT + 1]);
+
     return 0;
 }
